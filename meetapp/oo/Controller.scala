0d036cf89dbeaee28231631c7db40cfc7f675432
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2015-09-24 09:32:01 +0200

    add controller

---
+++ meetapp/oo/Controller.scala
@@ -0,0 +1,66 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+}
+// package controllers
+
+// import play.api._
+// import play.api.mvc._
+// import play.api.libs.json._
+// import play.api.Play.current
+// import play.api.db.slick.DB
+
+// import scala.concurrent.Future
+// import scala.concurrent.ExecutionContext
+// import play.api.libs.concurrent.Akka
+
+// import org.hablapps.meetup.{domain, db, logic}, 
+//   logic._,
+//   db._,
+//   domain._
+
+// object Members extends Controller{
+
+//   def add(gid: Int): Action[Int] =
+//     Action(parse.json[Int]) { 
+//       fromHTTP(gid) andThen 
+//       join          andThen
+//       interpreter   andThen
+//       toHTTP
+//     }
+
+//   def interpreter[U]: Store[U] => Either[StoreError, U] = 
+//     MySQLInterpreter.run[U]
+  
+//   def fromHTTP(gid: Int): Request[Int] => JoinRequest = 
+//     request => JoinRequest(None, request.body, gid)
+
+//   def toHTTP(response: Either[StoreError, JoinResponse]): Result = 
+//     response fold(
+//       error => error match {
+//         case error@NonExistentEntity(id) => 
+//           NotFound(s"${error.msg}")
+//         case error@ConstraintFailed(IsMember(_,_,_)) => 
+//           Forbidden(s"${error.msg}")
+//         case error@ConstraintFailed(IsPending(_,_,_)) => 
+//           Forbidden(s"${error.msg}")
+//         case error => 
+//           InternalServerError(error.msg)
+//       },
+//       success => success fold(
+//         joinRequest => 
+//           Accepted(s"Join request $joinRequest, left pending for futher processing"),
+//         member => 
+//           Created(Json.toJson(member)(Json.writes[Member]))
+//       )
+//     )
+
+// }
